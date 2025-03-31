d55fb0cd002d8849cce26b506ff947eee8e6943b
Navtej5064-creator <navtejreddy5064@gmail.com>
2025-03-31 15:42:40 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "devsecops-tetris-navtej" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-southeast-1"
   }
 }
